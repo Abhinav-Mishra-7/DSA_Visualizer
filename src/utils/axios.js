7f338d207ebb0e6a2cf4751cd541596056557fd1
Abhinav Mishra <abhinavmishra12514@gmail.com>
2026-04-01 00:39:19 +0530

    updated axios

--- src/utils/axios.js
+++ src/utils/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API_BASE_URL =
-  import.meta.env.VITE_API_BASE_URL || "http://localhost:3000";
+  import.meta.env.VITE_API_BASE_URL ;
 
 // 🔥 CREATE INSTANCE
 const axiosClient = axios.create({
